d1604ab248e4929e0ba1910979e645946617e0d7
Tom Blanchard <tom.blanchard@sureware.com>
2023-10-17 20:12:02 -0400

    In FileManagerController.cs
    Added A method to Read CSVs from Azure Blob Storage
    
    Data Transformation: The method reads the CSV blob and transforms its content into a list of dictionaries. This change is explained in the comments to clarify the data transformation process.
    
    Empty Blob Handling: The method now checks for an empty blob and returns a specific message if the blob contains no data. This behavior is documented in the comments.
    
    Error Handling: Improved error handling has been added to handle various types of exceptions, such as Azure Storage-related errors, HTTP errors, and other unexpected errors. This ensures that the API responds appropriately in different error scenarios.
    
    In Program.cs
    
    Optimized Swagger Configuration: The configuration of Swagger (OpenAPI) was simplified, and duplicate code for Swagger endpoints was removed.
    
    Optimized Azure Clients Registration: Azure client registration was simplified to remove duplicated configuration for BlobServiceClient, QueueServiceClient, and the use of the DefaultAzureCredential.

---
+++ NetCoreAzureBlobServiceAPI/Classes/AzuriteManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+namespace NetCoreAzureBlobServiceAPI.Classes
+{
+    public class AzuriteManager
+    {
+        private Process azuriteProcess;
+
+        public void StartAzurite()
+        {
+            azuriteProcess = new Process
+            {
+                StartInfo =
+            {
+                FileName = "azurite.cmd", // or the path to your Azurite executable
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+            };
+
+            azuriteProcess.Start();
+        }
+
+        public void StopAzurite()
+        {
+            if (azuriteProcess != null && !azuriteProcess.HasExited)
+            {
+                azuriteProcess.Kill();
+                azuriteProcess.WaitForExit();
+                azuriteProcess.Dispose();
+            }
+        }
+    }
+}
